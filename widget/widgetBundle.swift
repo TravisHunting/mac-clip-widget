1d32f6b39d9893ea3337e7b7c370b00467b0da1b
travis <travis.hunting@gmail.com>
2025-04-02 00:30:13 +1300

    widget

---
+++ widget/widgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  widgetBundle.swift
+//  widget
+//
+//  Created by Travis on 02/04/2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct widgetBundle: WidgetBundle {
+    var body: some Widget {
+        widget()
+    }
+}
