0e4b5a32b81cbb3025b624a877cdf5bdde4de330
travis <travis.hunting@gmail.com>
2025-04-02 00:12:30 +1300

    Initial Commit

---
+++ clippy/clippyApp.swift
@@ -0,0 +1,17 @@
+//
+//  clippyApp.swift
+//  clippy
+//
+//  Created by Travis on 02/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct clippyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ clippyTests/clippyTests.swift
@@ -0,0 +1,17 @@
+//
+//  clippyTests.swift
+//  clippyTests
+//
+//  Created by Travis on 02/04/2025.
+//
+
+import Testing
+@testable import clippy
+
+struct clippyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
