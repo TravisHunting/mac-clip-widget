eaa178b6de91ceb0b997a65ad4c212591154cb15
travis <travis.hunting@gmail.com>
2025-04-05 21:54:30 +1300

    app now completely invisible

--- clippy/clippyApp.swift
+++ clippy/clippyApp.swift
@@ -6,9 +6,28 @@
 //
 
 import SwiftUI
+import AppKit
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Hide dock icon
+        NSApp.setActivationPolicy(.accessory)
+    }
+    
+    // Handle URL scheme outside the UI
+    func application(_ application: NSApplication, open urls: [URL]) {
+        for url in urls {
+            if url.scheme == "clippy" && url.host == "screenshot" {
+                ScreenshotManager.takeScreenshotAndCopyToClipboard()
+            }
+        }
+    }
+}
 
 @main
 struct clippyApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
@@ -18,6 +37,18 @@ struct clippyApp: App {
                         ScreenshotManager.takeScreenshotAndCopyToClipboard()
                     }
                 }
+                .hidden() // Hide the window completely
+        }
+        .windowStyle(.hiddenTitleBar) // Hide title bar
+        .windowResizability(.contentSize) // Prevent window resizing
+        .commands {
+            // Remove standard menu commands
+            CommandGroup(replacing: .newItem) {}
+            CommandGroup(replacing: .pasteboard) {}
+            CommandGroup(replacing: .undoRedo) {}
+            CommandGroup(replacing: .textEditing) {}
+            CommandGroup(replacing: .windowList) {}
+            CommandGroup(replacing: .windowSize) {}
         }
     }
 }
